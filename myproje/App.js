86c23b5b76932d1beb0214ec69f5a5e1ab9d4b5e
tunay22 <50638690+tunay22@users.noreply.github.com>
2020-05-23 08:08:31 +0300

    Add files via upload

--- myproje/App.js
+++ myproje/App.js
@@ -1,60 +1,88 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, FlatList,TextInput } from 'react-native'
+
 
 export default class App extends Component {
   state = {
-    data: '',
-    dataCountry:'',
-    dataCountry2:'',
+    data: [],
+    gelendatafull :[],
+    text:'',
   }
+  
+
   componentDidMount() {
     fetch('https://api.covid19api.com/summary')
       .then(response => response.json())
       .then(user => user)
-      .then(user => this.setState({ data: user.Global, dataCountry: user.Countries[177],
-        dataCountry2: user.Countries[172]
-      }))    
+      .then(user => this.setState({ data: user.Countries , gelendatafull:user.Countries}))
   }
-  render() {
-    const { data , dataCountry, dataCountry2} = this.state;
+  searchFilter = text =>{
+    const newData = this.state.gelendatafull.filter(item => {
+      const ListItem =`${item.Country.toLowerCase()}`
+
+      return ListItem.indexOf(text.toLowerCase()) > -1;
+    });
+    this.setState({
+      data:newData,
+    });
+  };
+  renderHeader = () => {
+    const { text } = this.state;
+    return (
+      <View>
+        <TextInput
+          onChangeText={text =>{
+            this.searchFilter(text);
+            this.setState({
+              text,
+            });
+          }}
+          placeholder="ARAMA ...  " style={styles.search}></TextInput>
+      </View>
+    )
+  }
+
+  renderApiItem = ({ item, index }) => {
     return (
       <View style={styles.container}>
-        <View style={styles.vaka}>
-          <Text style={styles.baslık}>DÜNYA</Text>
-          <Text>Yeni vaka sayısı : {data.NewConfirmed} </Text>
-          <Text>Toplam vaka sayısı : {data.TotalConfirmed} </Text>
-          <Text style={styles.olum}>Yeni ölü sayısı : {data.NewDeaths} </Text>
-          <Text style={styles.olum}>Toplam ölü sayısı : {data.TotalDeaths} </Text>
+        <View>
+          <Text style={{ fontSize: 35 ,fontStyle:'italic'}}>{item.Country}</Text>
+          <View style ={styles.container2} >
+            <Text>Yeni vaka sayısı : {item.NewConfirmed}</Text>
+            <Text>Toplam vaka sayısı: {item.TotalConfirmed}</Text>
+            <Text style={styles.olum}>Bugün ölen sayısı : {item.NewDeaths}</Text>
+            <Text style={styles.olum}>Toplam ölen sayısı : {item.TotalDeaths}</Text>
+            <Text style={styles.iyilesen}>Bugün iyileşen sayısı : {item.NewRecovered}</Text>
+            <Text style={styles.iyilesen}>Toplam iyileşen sayısı : {item.TotalRecovered}</Text>
+            <Text>Tarih : {item.Date}</Text>
+          </View>
         </View>
-        <View style={styles.vaka}>
-          <Text style={styles.baslık}>AMERİKA</Text>
-          <Text>Yeni vaka sayısı : {dataCountry.NewConfirmed} </Text>
-          <Text>Toplam vaka sayısı : {dataCountry.TotalConfirmed} </Text>
-          <Text style={styles.olum}>Yeni ölü sayısı : {dataCountry.NewDeaths} </Text>
-          <Text style={styles.olum}>Toplam ölü sayısı : {dataCountry.TotalDeaths} </Text>
-          <Text style={styles.iyilesen}>Toplam iyileşen sayısı : {dataCountry.TotalRecovered} </Text>
-        </View>
-        <View style={styles.vaka}>
-          <Text style={styles.baslık}>TÜRKİYE</Text>
-          <Text>Yeni vaka sayısı : {dataCountry2.NewConfirmed} </Text>
-          <Text>Toplam vaka sayısı : {dataCountry2.TotalConfirmed} </Text>
-          <Text style={styles.olum}>Yeni ölü sayısı : {dataCountry2.NewDeaths} </Text>
-          <Text style={styles.olum}>Toplam ölü sayısı : {dataCountry2.TotalDeaths} </Text>
-          <Text style={styles.iyilesen}>Toplam iyileşen sayısı : {dataCountry2.TotalRecovered} </Text>
-        </View>
+      </View>
+    )
+  };
+  render() {
+    const { data } = this.state;
+    return (
+      <View style={styles.page}>
+        <FlatList
+          renderItem={this.renderApiItem}
+          ListHeaderComponent={this.renderHeader()}
+          keyExtractor={item => item.CountryCode}
+          data={data} />
       </View>
     )
   }
 }
 
 const styles = StyleSheet.create({
-  container:{
+  page:{
     flex:1,
-    backgroundColor:'#C0C6CC'
+    backgroundColor:'#E9D2E9',
   },
-  vaka:{
-    alignItems:'center',
-    paddingTop:50,
+  container: {
+    alignItems: 'baseline',
+    marginLeft: 10,
+    backgroundColor:'#E9D2E9',
   },
   olum:{
     color:'#AE0C0C'
@@ -62,7 +90,14 @@ const styles = StyleSheet.create({
   iyilesen:{
     color:'#3C9D54'
   },
-  baslık:{
-    color:'#195B97'
+  search: {
+    alignItems:'center',
+    marginLeft: 20,
+    padding: 10,
+    backgroundColor:'#FFFEFF',
+    width:370,
+    borderRadius:30,
+    marginTop:10,
   }
 })
+
